50ec4be62fe87df7f84c87eadb8020661e14d76e
Descaplis <92810856+Descaplis@users.noreply.github.com>
2025-09-17 13:39:35 +0200

    Inicjacja zmiennych

--- ProjektUl/Program.cs
+++ ProjektUl/Program.cs
@@ -1,8 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        
+        int beesCount;
+        int workersCount;
+        int guardsCount;
+        int dronesCount;
+        int honey;
+        int youngBees;
+
+        Console.WriteLine("Wpisz, ile chcesz mieć pszczół (min. 25):");
+        beesCount = Convert.ToInt32(Console.ReadLine());
+        while (beesCount < 25)
+        {
+            Console.WriteLine("Musisz mieć minimum 25 pszczół");
+            Console.WriteLine("Wpisz jeszcze raz:");
+            beesCount = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Liczenie, ile pszczół z każdego rodzaju, miodu i zapasu młodych pszczół
+        workersCount = Convert.ToInt32(Math.Ceiling(beesCount * 0.8));
+        int leftBees = beesCount - workersCount;
+        dronesCount = Convert.ToInt32(Math.Ceiling(leftBees * 0.6));
+        guardsCount = leftBees - dronesCount;
+
+        honey = beesCount * 10;
+        youngBees = Convert.ToInt32(Math.Ceiling(beesCount * 0.15));
+
+
+        DateTime SimulationStartDay = DateTime.Now;
     }
 }
